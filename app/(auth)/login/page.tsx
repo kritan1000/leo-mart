e6d1f343d4cb00807d2a9f00b5d7eb3022775d2c
kritan nepal <kritannepal11@gmail.com>
2026-05-25 20:11:38 +0545

    Initial Commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { LeoMartLogo } from "../_components/type/AuthComponent";
+import LoginFormZod from "./LoginFormZod/LoginFormZod";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <LoginFormZod />
+    </div>
+  );
+}
