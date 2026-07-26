d8dd494614db86a5235711a6b7b1336f5d4cb3e4
kritan nepal <kritannepal11@gmail.com>
2026-07-26 15:55:18 +0545

    chore: remove legacy root app auth pages and form components

--- app/(auth)/_components/type/AuthComponent.tsx
+++
@@ -1,7 +0,0 @@
-export function LeoMartLogo({ size = 28 }: { size?: number }) {
-  return (
-    <div style={{ fontSize: size }} className="font-bold text-[#4F46E5]">
-      LEO MART
-    </div>
-  );
-}
